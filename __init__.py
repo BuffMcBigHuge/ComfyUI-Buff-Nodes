3d0ac79aae406a634237efe64b2fe6440693faf1
BuffMcBigHuge <marco@bymar.co>
2025-03-01 18:24:34 -0500

    Small fix.

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .Buff import FilePathSelectorFromDirectory
+
+NODE_CLASS_MAPPINGS = {
+    "FilePathSelectorFromDirectory": FilePathSelectorFromDirectory,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "FilePathSelectorFromDirectory": "File Path Selector From Directory",
+}
